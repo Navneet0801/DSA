73c17321f55f631e13b920d11fc6722db3517a3a
Navneet0801 <nk9810328872@gmail.com>
2022-07-01 01:20:51 +0530

    tree implementation complete

--- binaryTree/binaryTree.cpp
+++ binaryTree/binaryTree.cpp
@@ -14,27 +14,111 @@ struct node{
     }
 };
 
-node * buildTree(node * root){
-    cout<<"Enter data: "<<endl;
-    int data;
-    cin>>data;
-    root = new node(data);
-
+node* buildTree(int data){
     if(data == -1){
-        return NULL;
+        return nullptr;
     }
 
-    cout<<"Enter data for the left of "<<data<<endl;
-    root->left = buildTree(root->left);
-    cout<<"Enter data for the right of "<<data<<endl;
-    root->right = buildTree(root->right);
+    node* root = new node(data);
 
-    return root;
+    cout<<"data for left of "<<data<<": ";
+    int ldata;
+    cin>>ldata;
+    cout<<"data for right of "<<data<<": ";
+    int rdata;
+    cin>>rdata;
+
+    root->left = buildTree(ldata);
+    root->right = buildTree(rdata);
+}
+
+//function for level order traverse
+void levelOrderTraversal(node* root){
+    queue<node*> q;
+    if (root) {
+        q.push(root);
+    }
+    while (!q.empty()) {
+        int size = q.size();
+        for (int i = 0; i < size; i++) {
+            node* front = q.front(); 
+            q.pop();
+            cout << front->data << ' ';
+            if (front->left) {
+                q.push(front->left);
+            }
+            if (front->right) {
+                q.push(front->right);
+            }
+        }
+        cout << endl; // change line after every level
+    }
+}
+
+//function for pre order traverse
+void preOrderTraversal(node* root){
+    if(root == nullptr){
+        return;
+    }
+
+    cout<<root->data<<" ";
+    preOrderTraversal(root->left);
+    preOrderTraversal(root->right);
+}
+
+//function for post order traverse
+void postOrderTraversal(node* root){
+    if(root == nullptr){
+        return;
+    }
+
+    postOrderTraversal(root->left);
+
+    postOrderTraversal(root->right);
+
+    cout<<root->data<<" ";
+}
+
+//function for inorder traverse
+void inOrderTraversal(node* root){
+    if(root == nullptr){
+        return;
+    }
+
+    inOrderTraversal(root->left);
+
+    cout<<root->data<<" ";
+
+    inOrderTraversal(root->right);
 }
 
 int main(){
-    node * root = NULL;
+    node * root;
+    cout<<"Enter data for root: ";
+    int rdata;
+    cin>>rdata;
 
-    //create a tree
-    buildTree(root);
+    root = buildTree(rdata);
+
+
+    //levelOrderTraversing
+    cout << "Printing the level order traversal output " << endl;
+    levelOrderTraversal(root);
+
+    //pre Order Traversal
+    cout << "Printing the pre order traversal output " << endl;
+    preOrderTraversal(root);
+    cout<<endl;
+
+    //post Order Traversal
+    cout << "Printing the post order traversal output " << endl;
+    postOrderTraversal(root);
+    cout<<endl;
+
+    //inorder Traversal
+    cout << "Printing the inorder traversal output " << endl;
+    inOrderTraversal(root);
+    cout<<endl;
+
+    return 0;
 }
