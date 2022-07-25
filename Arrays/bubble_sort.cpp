e83f260793ff7aff01a0ba02ea876294a05e4f7e
Navneet0801 <nk9810328872@gmail.com>
2022-07-25 19:32:46 +0530

    bubble_sort

---
+++ Arrays/bubble_sort.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void swap(int &a, int &b){
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+void bubbleSort(int arr[], int n){
+    for(int i=0; i<n-1; i++){
+        for(int j=0; j<n-i-1; j++){
+            if(arr[j] > arr[j+1]){
+                swap(arr[j], arr[j+1]);
+            }
+        }
+    }
+}
+
+int main(){
+    int arr[5] = {10, 40, 50, 20, 30};
+
+    bubbleSort(arr, 5);
+
+
+    for(auto i:arr){
+        cout<<i<<" ";
+    }
+
+    return 0;
+}
