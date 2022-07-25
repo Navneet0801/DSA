a660438947338d987930fd53ede3f1add79f2a3a
Navneet0801 <nk9810328872@gmail.com>
2022-07-25 14:00:27 +0530

    search in 2d array

---
+++ Arrays/searchin2dsortedmatrix.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool searchMatrix(vector<vector<int>>& matrix, int target) {
+     int r = 0, c = matrix[0].size()-1;
+        while(r < matrix.size() && c >= 0) {
+            if(matrix[r][c] == target) return true;
+            else if(matrix[r][c] < target) r++;
+            else c--;
+        }
+    return false;
+}
+
+int main(){
+    vector<vector<int>> vec = {{1,4,7,11,15},{2,5,8,12,19},{3,6,9,16,22},{10,13,14,17,24},{18,21,23,26,30}};
+
+    cout<<searchMatrix(vec, 5);
+
+    return 0;
+}
