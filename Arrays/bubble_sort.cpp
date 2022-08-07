96767abd25f80a576d44a4d8e3e77e0f44655685
Navneet0801 <nk9810328872@gmail.com>
2022-08-07 16:56:46 +0530

    bubble sort using recursion

--- Arrays/bubble_sort.cpp
+++ Arrays/bubble_sort.cpp
@@ -7,7 +7,25 @@ void swap(int &a, int &b){
     b = temp;
 }
 
-void bubbleSort(int arr[], int n){
+//with recursion
+void bubbleSort1(int arr[], int n){
+    if(n == 1 || n == 0){
+        return;
+    }
+
+    int j = 0;
+    while(j < n-1){
+        if(arr[j] > arr[j+1]){
+            swap(arr[j], arr[j+1]);
+        }
+        j++;
+    }
+    bubbleSort1(arr, n-1);
+}
+
+
+//without recursion
+void bubbleSort(int *arr, int n){
     for(int i=0; i<n-1; i++){
         for(int j=0; j<n-i-1; j++){
             if(arr[j] > arr[j+1]){
@@ -18,9 +36,10 @@ void bubbleSort(int arr[], int n){
 }
 
 int main(){
-    int arr[5] = {10, 40, 50, 20, 30};
+    int arr[8] = {45,23,1,26,58,79,45,12};
 
-    bubbleSort(arr, 5);
+    // bubbleSort(arr, 5);
+    bubbleSort1(arr, 8);
 
 
     for(auto i:arr){
